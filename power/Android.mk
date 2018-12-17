dd4964168ee37e8c9389818689d757317e05bf76
Xhoni Rexhepi <xhonirexhepi@gmail.com>
2018-12-17 08:58:27 +0000

    sdm845-common: power: Update to LA.UM.7.3.r1-06600-sdm845.0 tag

--- power/Android.mk
+++ power/Android.mk
@@ -6,25 +6,19 @@ ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
 endif
 
-LOCAL_MODULE := power.qcom
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SRC_FILES := \
-    hint-data.c \
-    list.c \
-    metadata-parser.c \
-    power-845.c \
-    power.c \
-    powerhintparser.c \
-    utils.c
-
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
+LOCAL_HEADER_LIBRARIES += libutils_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_SRC_FILES := power-845.c power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c
 LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
 
-LOCAL_CFLAGS += -Werror -Wall -Wno-unused-parameter
-LOCAL_CFLAGS += -DINTERACTION_BOOST
-
+LOCAL_MODULE := power.qcom
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 include $(BUILD_SHARED_LIBRARY)
